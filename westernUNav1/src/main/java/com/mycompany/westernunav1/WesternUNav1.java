8adec839b40a76e342643692fc6777922864b6d0
Tanusri Harish <tharish@uwo.ca>
2023-03-06 18:27:08 -0500

    Initial Commit

---
+++ westernUNav1/src/main/java/com/mycompany/westernunav1/WesternUNav1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.westernunav1;
+
+/**
+ *
+ * @author tanusri
+ */
+public class WesternUNav1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
