4107bfe4cb104baef3c95bb3fc54cb9b4964cf79
Tanusri Harish <tharish@uwo.ca>
2023-03-06 21:20:02 -0500

    Required classes

---
+++ westernUNav1/src/main/java/com/mycompany/westernunav1/User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.westernunav1;
+
+/**
+ *
+ * @author tanusri
+ */
+public class User {
+    
+}
