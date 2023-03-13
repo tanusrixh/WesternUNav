1d5a6a921de666ab5b1bd1c158c0a9329141a1b1
nfayadh <nfayadh@uwo.ca>
2023-03-12 21:03:43 -0400

    This is my initial commit!

--- westernUNav1/src/main/java/com/mycompany/westernunav1/Building.java
+++ westernUNav1/src/main/java/com/mycompany/westernunav1/Building.java
@@ -8,7 +8,74 @@ import javax.swing.*;
 /**
  *
  * @author tanusri
+ * @author nour
  */
 public class Building {
+    
+    /** The building's name*/
+    private String name;
+    
+    /** The building's number of floors*/
+    private int floors;
+    
+    /** An array of the building's rooms*/
+    private Room[] rooms;
+    
+    
+    /** Building constructor, creates a new Building object
+     * @param name the building's name
+     * @param floors the number of floors
+     * @param rooms an array of the building's rooms
+     */
+    public Building(String name, int floors, Room[] rooms){
+        
+        this.name = name;
+        this.floors = floors;
+        this.rooms = rooms;
+    }
+    
+    
+    /** A getter method to get the name of the building
+     * @return the building's name
+     */
+    public String getName(){
+        return name;
+    }
+    
+    /** A setter method to set the name of the building
+     * @param name the building's name
+     */
+    public void setName(String name){
+        
+        this.name = name;
+    }
+    
+    /** A getter method to get the number of floors in the building
+     * @return the number of floors in the building
+     */
+    public int getFloors(){
+        return floors;
+    }
+    
+    /** A setter method to set the number of floors in the building
+     * @param floors the building's number of floors
+     */
+    public void setFloors(int floors){
+        this.floors = floors;
+    }
+    
+    /** A getter method to get the rooms in the building
+     * @return the rooms in the building
+     */
+    public Room[] getRooms(){
+        return rooms;
+    }
+    
+    /** A setter method to set the rooms in the building
+     * @param rooms the building's rooms
+     */
+    public void setRooms(Room[] rooms){
+        this.rooms = rooms;
+    }
     
 }
