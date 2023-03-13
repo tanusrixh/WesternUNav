f71c1185b28b5ce2fb5a542b32a855dd40779293
nsobhan <nsobhan@uwo.ca>
2023-03-13 17:22:31 -0400

    This is my third commit

--- westernUNav1/src/main/java/com/mycompany/westernunav1/PointofInterest.java
+++ westernUNav1/src/main/java/com/mycompany/westernunav1/PointofInterest.java
@@ -10,31 +10,37 @@ import javax.swing.*;
  * @author Nosheen
  */
 public class PointofInterest {
-    
+    /*private variables*/
     private String poiname;
     private String poidescription;
     private String poilocation;
     
+    /*get point of interest name*/
     public String getPoiname() {
         return poiname;
     }
 
+    /*set point of interest name*/
     public void setPoiname(String poiname) {
         this.poiname = poiname;
     }
-
+    
+    /*get point of interest description*/
     public String getPoidescription() {
         return poidescription;
     }
 
+    /*set point of interest description*/
     public void setPoidescription(String poidescription) {
         this.poidescription = poidescription;
     }
-
+    
+    /*get point of interest location*/
     public String getPoilocation() {
         return poilocation;
     }
 
+    /*set point of interest location*/
     public void setPoilocation(String poilocation) {
         this.poilocation = poilocation;
     }
