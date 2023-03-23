86a5b1ed3704152b5848745b288cca9d41a7f7e0
habudaqq <habudaqq@uwo.ca>
2023-03-23 17:04:14 -0400

    Added a LinkedList for Points of Interest

--- westernUNav1/src/main/java/com/mycompany/westernunav1/Floor.java
+++ westernUNav1/src/main/java/com/mycompany/westernunav1/Floor.java
@@ -4,29 +4,52 @@
  */
 package com.mycompany.westernunav1;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import javax.swing.*;
 
 /**
  *
  * @author Hala Abudaqqa
+ * 
+ * A floor has a floor number and contains rooms and points of interest.
+ * Rooms and points of interest can be added or removed from the floor.
  */
 public class Floor {
     
-   
-    // two instance variables
+    /**The floor number of the floor*/
     private int floorNumber;
-    private ArrayList<Room> rooms;  // arraylist of Room objects
     
-    // constructor
+    /**The list of rooms on the floor*/
+    private ArrayList<Room> rooms;
+    
+    /**The list of points of interest on the floor*/
+    private LinkedList<PointofInterest> pointofInterest;
+    
+    /**
+     * Constructs a new floor with the given floor number.
+     * 
+     * @param floorNumber the floor number of the new floor.
+     */
     public Floor(int floorNumber) {
         this.floorNumber = floorNumber;
         this.rooms = new ArrayList<Room>();
+        this.pointofInterest = new LinkedList<PointofInterest>();
     }
     
+    /**
+     * Returns the floor number of the floor.
+     * 
+     * @return the floor number of the floor.
+     */
     public int getNumber(){
         return this.floorNumber;
     }
     
+    /**
+     * Returns an array of all the rooms on the floor.
+     * 
+     * @return an array of all the rooms on the floor.
+     */
     public Room[] getRooms(){
         // Create a new Room array with the same length as the rooms ArrayList
         Room[] roomsArray = new Room[this.rooms.size()];
@@ -38,11 +61,48 @@ public class Floor {
         return roomsArray;
     }
     
+    /**
+     * Adds a room to the floor.
+     * 
+     * @param room the room to add to the floor.
+     */
     public void addRoom(Room room){
         this.rooms.add(room);
     }
     
+    /**
+     * Removes a room from the floor.
+     * 
+     * @param room the room to remove from the floor.
+     */
     public void removeRoom(Room room){
         this.rooms.remove(room);
+    }
+    
+    /**
+     * Gets a list of points of interest on the floor.
+     *
+     * @return a list of points of interest on the floor
+     */
+    public LinkedList<PointofInterest> getPointsOfInterest(){
+        return this.pointofInterest;
+    }
+    
+    /**
+     * Adds a point of interest to the floor.
+     *
+     * @param poi the point of interest to add
+     */
+    public void addPointOfInterest(PointofInterest poi){
+        this.pointofInterest.add(poi);
+    }
+    
+    /**
+     * Removes a point of interest from the floor.
+     *
+     * @param poi the point of interest to remove
+     */
+    public void removePointOfInterest(PointofInterest poi){
+        this.pointofInterest.remove(poi);
     }
 }
