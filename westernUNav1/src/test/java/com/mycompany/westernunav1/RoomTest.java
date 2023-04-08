68bad54c8cb1d4615c6a13c14f5a0f7b86100f91
nsobhan <nsobhan@uwo.ca>
2023-04-08 17:20:37 -0400

    created room test file and passed all tests

--- westernUNav1/src/test/java/com/mycompany/westernunav1/RoomTest.java
+++ westernUNav1/src/test/java/com/mycompany/westernunav1/RoomTest.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
+
 package com.mycompany.westernunav1;
 
 import org.junit.jupiter.api.AfterEach;
